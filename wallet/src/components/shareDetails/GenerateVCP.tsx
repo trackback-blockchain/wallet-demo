04998aba8488310966c24f934a68b3f5384239a0
Don <don@track-back.co>
2021-08-19 10:53:11 +1200

    qr validation and new screens

---
+++ wallet/src/components/shareDetails/GenerateVCP.tsx
@@ -0,0 +1,20 @@
+
+import { ReactComponent as Logo } from '../logo.svg';
+
+import './sharingAccess.scss'
+
+function GenerateVCP() {
+
+    return (
+        <div className="container-sharing-access">
+
+            <div className="logo-container">
+                <Logo width="77" height="77" />
+            </div>
+            <div className="title">Please wait...</div>
+        </div>
+    )
+
+}
+
+export default GenerateVCP
